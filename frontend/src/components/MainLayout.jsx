4c5e4b047ce6cc93b3accf2bb10fbe8d37ac516f
Abhishek <abhishekbhi736@gmail.com>
2026-08-08 20:25:03 +0530

    fix: import useLocation from react-router-dom in MainLayout.jsx

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import EditTargetModal from './EditTargetModal';
